abd90da1cf6a1d9a69c8acb459c22e7867bbae85
David Huang <hjc@hjc.im>
2023-01-13 01:41:39 +0800

    Use larger buffer & output csv

--- Program.cs
+++ Program.cs
@@ -14,11 +14,13 @@ namespace InterCoreBench
     {
         const int TestPeriodInMs = 5000;
         const int TestIntervalInMs = 1000;
-        const int TestCopyBlockSize = 128 * 1024;
+        const int TestCopyBlockSize = 256 * 1024;
         const bool EnableReverseBandwidthTest = false; // Maybe useful in HMP systems
 
         static IThreadAffinity ThreadAffinity;
         static volatile bool cancel = false;
+        static int[,] latencyResultsNs;
+        static ulong[,] bandwidthResultsMBpersec;
 
         static (ulong count, TimeSpan elapsed) DoSync(int core, SemaphoreSlim wait, SemaphoreSlim notify, bool doCount)
         {
@@ -81,7 +83,7 @@ namespace InterCoreBench
             }
         }
 
-        static void TestSync(int c1, int c2)
+        static void TestSync(int c1, int c2, int p1, int p2)
         {
             using (var s1 = new SemaphoreSlim(1))
             using (var s2 = new SemaphoreSlim(0))
@@ -96,10 +98,12 @@ namespace InterCoreBench
                 t2.GetAwaiter().GetResult();
 
                 Console.WriteLine($"{elapsed.TotalMilliseconds * 1000 * 1000 / count / 2:0} ns ({count} synchronizations in {elapsed.TotalMilliseconds:0} ms)");
+                latencyResultsNs[p1, p2] = (int)(elapsed.TotalMilliseconds * 1000 * 1000 / count / 2);
+                latencyResultsNs[p2, p1] = (int)(elapsed.TotalMilliseconds * 1000 * 1000 / count / 2);
             }
         }
 
-        static unsafe void TestCopy(int c1, int c2)
+        static unsafe void TestCopy(int c1, int c2, int p1, int p2)
         {
             using (var s1 = new SemaphoreSlim(1))
             using (var s2 = new SemaphoreSlim(0))
@@ -116,6 +120,11 @@ namespace InterCoreBench
                 t2.GetAwaiter().GetResult();
 
                 Console.WriteLine($"{(double)count * TestCopyBlockSize / 1024 / 1024 / 1024 / elapsed.TotalSeconds:0.00} GB/s ({(double)count * TestCopyBlockSize / 1024 / 1024 / 1024:0.00} GB copied in {elapsed.TotalMilliseconds:0} ms)");
+                bandwidthResultsMBpersec[p1, p2] = (ulong)(count * TestCopyBlockSize / 1024 / 1024 / elapsed.TotalSeconds);
+                if (!EnableReverseBandwidthTest)
+                {
+                    bandwidthResultsMBpersec[p2, p1] = (ulong)(count * TestCopyBlockSize / 1024 / 1024 / elapsed.TotalSeconds);
+                }
             }
         }
 
@@ -138,24 +147,62 @@ namespace InterCoreBench
             }
 
             var physicalCores = logicalCoreInfo.GetPhysicalCoreIndex();
-
+            latencyResultsNs = new int[physicalCores.Count, physicalCores.Count];
+            bandwidthResultsMBpersec = new ulong[physicalCores.Count, physicalCores.Count];
             for (var i = 0; i < physicalCores.Count - 1; i++)
             {
                 for (var j = i + 1; j < physicalCores.Count; j++)
                 {
-                    TestSync(physicalCores[i], physicalCores[j]);
+                    TestSync(physicalCores[i], physicalCores[j], i, j);
                     Thread.Sleep(TestIntervalInMs);
-                    TestCopy(physicalCores[i], physicalCores[j]);
+                    TestCopy(physicalCores[i], physicalCores[j], i, j);
                     Thread.Sleep(TestIntervalInMs);
                     if (EnableReverseBandwidthTest)
                     {
-                        TestCopy(physicalCores[j], physicalCores[i]);
+                        TestCopy(physicalCores[j], physicalCores[i], j, i);
                         Thread.Sleep(TestIntervalInMs);
                     }
                 }
             }
 
+            Console.WriteLine("Latency results (ns)");
             Console.WriteLine();
+            Console.WriteLine("Core ID," + string.Join(',', physicalCores));
+
+            for (var i = 0; i < latencyResultsNs.GetLength(0); i++)
+            {
+                Console.Write(physicalCores[i]);
+                for (var j = 0; j < latencyResultsNs.GetLength(1); j++)
+                {
+                    Console.Write("," + latencyResultsNs[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Bandwidth results (MB/s)");
+            Console.WriteLine();
+            Console.WriteLine("Core ID," + string.Join(',', physicalCores));
+
+            for (var i = 0; i < bandwidthResultsMBpersec.GetLength(0); i++)
+            {
+                Console.Write(physicalCores[i]);
+                for (var j = 0; j < bandwidthResultsMBpersec.GetLength(1); j++)
+                {
+                    if (bandwidthResultsMBpersec[i, j] > 0)
+                    {
+                        Console.Write("," + bandwidthResultsMBpersec[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write(",");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
             Console.WriteLine("Test finished, press any key to exit.");
             Console.ReadKey();
         }
